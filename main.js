cc61e2ec57022ef71d6c93e621b2474ba361e9fb
slnturk <sln.turk98@gmail.com>
2024-12-04 21:00:12 +0300

    Fin Page

--- main.js
+++ main.js
@@ -7,3 +7,29 @@ videoElement.addEventListener('ended', () => {
     videoElement.classList.add('d-none')
     document.getElementById('endframe').classList.remove('d-none')
 })
+
+Array.from(document.querySelectorAll('.btn.border')).forEach(button => {
+    if (button.classList.contains('border-light')) {
+        button.addEventListener('mouseover',()=>{
+            button.classList.toggle('border-light')
+            button.classList.toggle('border')
+            button.classList.toggle('btn-light')
+        })
+        button.addEventListener('mouseout',()=>{
+            button.classList.toggle('border-light')
+            button.classList.toggle('border')
+            button.classList.toggle('btn-light')
+        })
+    } else if(button.classList.contains('border-primary')) {
+        button.addEventListener('mouseover',()=>{
+            button.classList.toggle('border-primary')
+            button.classList.toggle('border')
+            button.classList.toggle('btn-primary')
+        })
+        button.addEventListener('mouseout',()=>{
+            button.classList.toggle('border-primary')
+            button.classList.toggle('border')
+            button.classList.toggle('btn-primary')
+        })
+    }
+});
